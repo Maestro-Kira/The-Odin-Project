baf4aabace7e816a00c72fddb3cb2f661c8902f3
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-10-01 01:06:01 +0300

    style(ui): update visual style of the calculator.

--- Calculator/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-        // Remove the 'btn-clicked' class from all buttons first
-        document.querySelectorAll('.btn').forEach(btn => btn.classList.remove('btn-clicked'));
-
-        // Add the 'btn-clicked' class to the clicked button
-        button.classList.add('btn-clicked');
-    });
-});
