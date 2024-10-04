a66f58e071212a893b75375f48932a93d0063662
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2024-10-05 00:51:29 +0300

    feat(styles): add responsive CSS and button animation for mobile/tablet

--- Library/script.js
+++ Library/script.js
@@ -1,7 +1,28 @@
 let Library = [];
-
-function book(title, author, pagesAmount, status, genre) {
-
+const form = document.getElementById('libraryForm');
+function Book(title, status, author, pagesAmount, genre) {
+    this.title = title;
+    this.status = status;
+    this.author = author;
+    this.pagesAmount = pagesAmount;
+    this.genre = genre;
 }
+function addBookToLibrary(book) {
+    return Library.push(book);
+}
+
+form.addEventListener('submit', function(ev){
+    ev.preventDefault();
+    let bookTitle = document.getElementById('bookTitle').value;
+    let bookStatus = document.getElementById('bookStatus').checked;
+    let bookAuthor = document.getElementById('bookAuthor').value;
+    let pagesAmount = document.getElementById('pagesAmount').value;
+    let genre = document.getElementById('bookGenre').value;
+
+    let newBook = new Book(bookTitle, bookStatus, bookAuthor, pagesAmount, genre);
+
+    addBookToLibrary(newBook);
+    console.log(Library)
+})
 
 
